28494c09d1c45ee2c71b5dd80257a9be6c0013c5
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2025-03-10 09:57:23 +0800

    implemented user profile data

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,14 +3,19 @@ import UserModel from "../model/UserModel.js";
 export const findUser = async (req, res) => {
   const userId = req.user.userId;
   try {
-    const user = await UserModel.findById(userId);
-    console.log(user, userId);
+    const user = await UserModel.findById(userId).select("-password");
+  
     if (!user) {
       return res.status(404).json({
         success: false,
         message: "User not found",
       });
     }
+
+    res.status(200).json({
+        success: true,
+        data: user,
+    })
   } catch (err) {
     return res.status(500).json({
       success: false,
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { findUser } from "../controller/userController.js";
+import authenticate, { authorizeRole } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/profile", findUser);
+userRouter.get("/profile", authenticate, findUser);
 
 export default userRouter;
-//
