580b26f6ea702c0649f8fda1941242227c35d2d7
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2025-03-17 11:35:47 +0800

    implemented violation chart in homepage

---
+++ frontend/src/components/home/StatCard.jsx
@@ -0,0 +1,42 @@
+import { motion } from "framer-motion";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const StatCard = ({ name, icon: Icon, value, color, loading, statuses=[] }) => {
+  return (
+    <motion.div
+      className="border shadow-sm border-slate-200 overflow-hidden rounded-xl"
+      //   whileHover={{ y: -5, boxShadow: "0 15px 20px -12px rgba(0, 0, 0, 0.5)" }}
+    >
+      {loading ? (
+        <Skeleton className={"h-[100px]"} />
+      ) : (
+        <div className="md:px-4 md:py-5 p-6">
+          <span className="flex items-center text-sm font-medium ">
+            <Icon size={20} className="mr-2" style={{ color }} />
+            {name}
+          </span>
+          <p className="mt-1 text-2xl font-medium ">{value}</p>
+         {/* Render statuses if available */}
+         {statuses.length > 0 && (
+            <div className="space-x-2 mt-2">
+              {statuses.map((status, index) => (
+                <span
+                  key={index}
+                  className="font-semibold text-xs py-1 px-2 rounded-md"
+                  style={{
+                    color: status.color,
+                    backgroundColor: status.bgColor,
+                  }}
+                >
+                  {status.label}
+                </span>
+              ))}
+            </div>
+          )}
+
+        </div>
+      )}
+    </motion.div>
+  );
+};
+export default StatCard;
